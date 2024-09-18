bd0d483cba51f16d583fca23f479911f248e7b41
felipery <feliperamirezyepes@gmail.com>
2024-09-18 18:12:39 -0500

    Se crea proyecto Calculadora

---
+++ 02-Calculadora/src/model/Calculadora.java
@@ -0,0 +1,7 @@
+package model;
+
+public abstract class Calculadora {
+
+    // This method is abstract, so it must be implemented in the subclasses
+    public abstract double operar(double number1, double number2);
+}
---
+++ 02-Calculadora/src/model/Suma.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Suma extends Calculadora {
+    @Override
+    public double operar(double number1, double number2) {
+        return number1 + number2;
+    }
+}
